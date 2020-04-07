81437ef480183ab3ad0c3cab643ade3da726c87a
DexmelH <57649261+DexmelH@users.noreply.github.com>
2020-04-07 11:31:19 +0800

    login

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require("cors");
+var bodyParser = require("body-parser")
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,6 +15,10 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+app.use(bodyParser.json())
+app.use(cors())
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.use(logger('dev'));
 app.use(express.json());
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,48 @@
-var express = require('express');
-var router = express.Router();
+const express =  require("express")
+const users = express.Router()
+const cors = require("cors")
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const user = require("..models/users")
+users.user(cors())
 
-module.exports = router;
+process.env.SECRET_KEY = 'secret'
+
+users.post('/register', (req, res) => {
+  const today = new Date()
+  const userData = {
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email,
+    password: req.body.password,
+    created: today,
+  }
+
+  user.findOne({
+    where: {
+      email: req.body.email
+    }
+  })
+  .then(user => {
+    if(!user) {
+      bcrypt.hash(req.body.password, 10, (err, hash) => {
+        userData.password = hash
+        user.create(userData)
+        .then(user => {
+          res.json({status: user.email + ' registered'})
+        })
+        .catch(err => {
+          res.send('error: ' + err)
+        })
+      })
+    } else {
+      res.json({error: "User already exists"})
+    }
+  })
+  .catch(err => {
+    res.send('error: ' + err)
+  })
+})
+
+module.exports = users
